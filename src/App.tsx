13ebf3eda1c3da4034ac340b358beafc16988471
GeorgianBadita <geo.badita@gmail.com>
2020-11-22 23:21:36 +0000

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,7 @@ const App = (): JSX.Element => {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <p>Hello from V2</p>
         <a
           className="App-link"
           href="https://reactjs.org"
