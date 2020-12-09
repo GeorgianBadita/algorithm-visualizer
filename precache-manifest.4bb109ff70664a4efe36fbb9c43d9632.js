6dbef84feafdd8d084c3861710e4992d95f66b55
GeorgianBadita <geo.badita@gmail.com>
2020-12-09 23:30:36 +0000

    Updates

---
+++ precache-manifest.4bb109ff70664a4efe36fbb9c43d9632.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08a667018106ec1d7e436a652fd9ecc8",
+    "url": "/algorithm-visualizer/index.html"
+  },
+  {
+    "revision": "007e359ea9b3bf05072e",
+    "url": "/algorithm-visualizer/static/css/2.9db35e51.chunk.css"
+  },
+  {
+    "revision": "3f54e5ae2b653d1c4b24",
+    "url": "/algorithm-visualizer/static/css/main.fbef01e9.chunk.css"
+  },
+  {
+    "revision": "007e359ea9b3bf05072e",
+    "url": "/algorithm-visualizer/static/js/2.a37c7e25.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/algorithm-visualizer/static/js/2.a37c7e25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f54e5ae2b653d1c4b24",
+    "url": "/algorithm-visualizer/static/js/main.160a4413.chunk.js"
+  },
+  {
+    "revision": "73842d6d03bf4c5ec262",
+    "url": "/algorithm-visualizer/static/js/runtime-main.19db2af3.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/algorithm-visualizer/precache-manifest.31af0ed18b522939910163a7ea87ea49.js"
+  "/algorithm-visualizer/precache-manifest.4bb109ff70664a4efe36fbb9c43d9632.js"
 );
 
 self.addEventListener('message', (event) => {
