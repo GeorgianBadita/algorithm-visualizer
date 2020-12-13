83662650a0e363b3171c6b91a7d3f8f8c61e6fff
GeorgianBadita <geo.badita@gmail.com>
2020-12-13 00:31:19 +0000

    Adjust table proportions

--- src/hooks/hooks.ts
+++ src/hooks/hooks.ts
@@ -4,13 +4,13 @@ const DEFAULT_SQUARE_SIZE = 30;
 
 export const useWindowSize = (): number[] => {
     const [size, setSize] = React.useState([
-        Math.round((window.innerWidth - 0.1 * window.innerWidth) / DEFAULT_SQUARE_SIZE),
+        Math.round((window.innerWidth - 0.12 * window.innerWidth) / DEFAULT_SQUARE_SIZE),
         Math.round((window.innerHeight - 0.32 * window.innerHeight) / DEFAULT_SQUARE_SIZE),
     ]);
     React.useLayoutEffect(() => {
         function updateSize() {
             setSize([
-                Math.round((window.innerWidth - 0.1 * window.innerWidth) / DEFAULT_SQUARE_SIZE),
+                Math.round((window.innerWidth - 0.12 * window.innerWidth) / DEFAULT_SQUARE_SIZE),
                 Math.round((window.innerHeight - 0.32 * window.innerHeight) / DEFAULT_SQUARE_SIZE),
             ]);
         }
