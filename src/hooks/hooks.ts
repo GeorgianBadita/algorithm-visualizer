b644aade645ed6d7aae77840ec75ab029e5ddf6e
GeorgianBadita <geo.badita@gmail.com>
2020-12-13 00:00:37 +0000

    Revert table proportions

--- src/hooks/hooks.ts
+++ src/hooks/hooks.ts
@@ -11,7 +11,7 @@ export const useWindowSize = (): number[] => {
         function updateSize() {
             setSize([
                 Math.round((window.innerWidth - 0.1 * window.innerWidth) / DEFAULT_SQUARE_SIZE),
-                Math.round((window.innerHeight - 0.31 * window.innerHeight) / DEFAULT_SQUARE_SIZE),
+                Math.round((window.innerHeight - 0.32 * window.innerHeight) / DEFAULT_SQUARE_SIZE),
             ]);
         }
         window.addEventListener('resize', updateSize);
